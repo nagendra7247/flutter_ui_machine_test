92b6b1f1260cc86583e1ab5d18580d14e798b767
ankit thakur <ankit@M1s-MacBook-Pro.local>
2025-08-03 11:54:36 +0530

    Added theme, folder structure, and home screen

---
+++ lib/themes/app_theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData lightTheme = ThemeData(
+    primarySwatch: Colors.indigo,
+    scaffoldBackgroundColor: Colors.white,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Colors.indigo,
+      foregroundColor: Colors.white,
+    ),
+  );
+}
